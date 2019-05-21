5cc64aa1c2e41c78b3d0645ccd743edc5f1c64e2
Reina Ewing <reina.castro3@gmail.com>
2019-05-21 02:51:43 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,4 +1,4 @@
-def my_hash
+def my_hash = {"name" => "Reina", "age" => "36"}
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 
   
